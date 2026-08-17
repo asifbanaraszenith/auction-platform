6ca5741ab870faade250908da161cb4f7ae9ffa2
asifbanaraszenith <asifbanaraszenith@users.noreply.github.com>
2026-08-18 00:00:19 +0500

    fix: make auction management route dynamic

---
+++ src/app/auctions/page.tsx
@@ -0,0 +1,7 @@
+import AuctionManagementClient from "./AuctionManagementClient";
+
+export const dynamic = "force-dynamic";
+
+export default function AuctionsPage() {
+  return <AuctionManagementClient />;
+}
