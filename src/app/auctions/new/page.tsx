e2cb2c582200d50c80a134cc8a20db8896ce78bb
asifbanaraszenith <asifbanaraszenith@users.noreply.github.com>
2026-09-23 13:55:21 +0500

    feat(auctions): add new auction route

---
+++ src/app/auctions/new/page.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+import AuctionManagementClient from "../AuctionManagementClient";
+
+export default function NewAuctionPage() {
+  return <AuctionManagementClient mode="detail" auctionId="new" />;
+}
